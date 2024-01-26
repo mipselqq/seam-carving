dbf5f8b7c29c57cd3acd69e9ba9b908f242f0286
Ilya <mipselqq@gmail.com>
2024-01-27 00:59:31 +0300

    update: kinda implement the sobel filter

--- src/energy_map.rs
+++ src/energy_map.rs
@@ -1,33 +1,36 @@
 use image::{Pixel, Rgb, RgbImage};
 
 pub fn calculate_energy_map(image: &RgbImage) -> RgbImage {
-    let (width, height) = image.dimensions();
-    let mut output_image = RgbImage::new(width, height);
+    let (image_width, image_height) = image.dimensions();
+    let mut energy_map = RgbImage::new(image_width, image_height);
 
-    for x in 0..(width - 1) {
-        for y in 0..height {
-            let current_pixel_color = image.get_pixel(x, y).0;
-            let next_pixel_color = image.get_pixel(x + 1, y).0;
-            let mut current_pixel_energy = 0i16;
+    for y in 1..(image_height - 1) {
+        for x in 1..(image_width - 1) {
+            let mut gradient_x = 0i16;
+            let mut gradient_y = 0i16;
 
-            // TODO: use more neighbours (including y axis) to calculate the enery
-            // or just find a better formula like sobel filter
-            for channel in 0..3 {
-                let current_pixel_channel = current_pixel_color[channel];
-                let next_pixel_channel = next_pixel_color[channel];
+            for x_offset in 0..3 {
+                for y_offset in 0..3 {
+                    let pixel_intensity = image.get_pixel(x + x_offset as u32 - 1, y + y_offset as u32 - 1).to_luma()[0] as i16;
 
-                current_pixel_energy += (next_pixel_channel as i16 - current_pixel_channel as i16).abs();
+                    gradient_x += calculate_sobel_coefficient(x_offset) * pixel_intensity;
+                    gradient_y += calculate_sobel_coefficient(y_offset) * pixel_intensity;
+                }
             }
 
-            let energy_representation_color = Rgb([
-                current_pixel_energy as u8,
-                current_pixel_energy as u8,
-                current_pixel_energy as u8,
-            ]);
+            let energy = (gradient_x.abs().max(gradient_y.abs()) as f32).clamp(0.0, 255.0) as u8;
 
-            output_image.put_pixel(x, y, energy_representation_color);
+            energy_map.put_pixel(x, y, Rgb([energy, energy, energy]));
         }
     }
 
-    output_image
+    energy_map
+}
+
+fn calculate_sobel_coefficient(axis_offset: i16) -> i16 {
+    match axis_offset {
+        0 => -1,
+        1 => 0,
+        _ => 1,
+    }
 }
