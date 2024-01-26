b58c3447981cc6958ed6d25ce6dc63ab2ea060b9
Ilya <mipselqq@gmail.com>
2024-01-27 00:37:47 +0300

    update: the energy map is now consists of many intermediate colors instaed of the black and white

--- src/energy_map.rs
+++ src/energy_map.rs
@@ -1,7 +1,5 @@
 use image::{Pixel, Rgb, RgbImage};
 
-const DELTA_TRESHOLD: i16 = 15;
-
 pub fn calculate_energy_map(image: &RgbImage) -> RgbImage {
     let (width, height) = image.dimensions();
     let mut output_image = RgbImage::new(width, height);
@@ -12,18 +10,20 @@ pub fn calculate_energy_map(image: &RgbImage) -> RgbImage {
             let next_pixel_color = image.get_pixel(x + 1, y).0;
             let mut current_pixel_energy = 0i16;
 
+            // TODO: use more neighbours (including y axis) to calculate the enery
+            // or just find a better formula like sobel filter
             for channel in 0..3 {
                 let current_pixel_channel = current_pixel_color[channel];
                 let next_pixel_channel = next_pixel_color[channel];
 
-                current_pixel_energy += next_pixel_channel as i16 - current_pixel_channel as i16;
+                current_pixel_energy += (next_pixel_channel as i16 - current_pixel_channel as i16).abs();
             }
 
-            let energy_representation_color = if current_pixel_energy < DELTA_TRESHOLD.into() {
-                Rgb([0, 0, 0])
-            } else {
-                Rgb([255, 255, 255])
-            };
+            let energy_representation_color = Rgb([
+                current_pixel_energy as u8,
+                current_pixel_energy as u8,
+                current_pixel_energy as u8,
+            ]);
 
             output_image.put_pixel(x, y, energy_representation_color);
         }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod energy_map;
 use energy_map::calculate_energy_map;
 
 fn main() {
-    let source_image_path = "tower.jpg";
+    let source_image_path = "arch.jpg";
     let source_image = image::open(source_image_path).expect("Failed to read the image").into_rgb8();
     let energy_map = calculate_energy_map(&source_image);
 
