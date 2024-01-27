c9e299dc07036670a9b3887065f839abd9a07508
Ilya <mipselqq@gmail.com>
2024-01-27 16:45:31 +0300

    I can't do it for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 mod energy_map;
+mod seam_removal;
 use std::{path::Path, time::Instant};
 
 use energy_map::calculate_energy_map;
 
+use crate::seam_removal::remove_vertical_seam;
+
+const TARGET_WIDTH: u32 = 1000;
+
 fn main() {
     let source_image_path = Path::new("./images/tower.jpg");
-    let source_image = image::open(source_image_path).expect("Failed to read the image").into_rgb8();
+    let mut image = image::open(source_image_path).expect("Failed to read the image").into_rgb8();
 
     let energy_map_calculation_time = Instant::now();
-    let energy_map = calculate_energy_map(&source_image);
+    let mut energy_map = calculate_energy_map(&image);
     println!("Energy map is calculated in {}ms", energy_map_calculation_time.elapsed().as_millis());
 
-    energy_map.save("energy_map.png").expect("Cannot save the energy map");   
+    for i in 0..(image.width() - TARGET_WIDTH)  {
+        remove_vertical_seam(&mut image, &energy_map);
+        energy_map = calculate_energy_map(&image);
+        dbg!("removed", i);
+    }
+
+    image.save("vertical_carving.png").unwrap();
 }
---
+++ src/seam_removal.rs
@@ -0,0 +1,3 @@
+use image::{GrayImage, Rgb, RgbImage};
+
+pub fn remove_vertical_seam(image: &mut RgbImage, energy_map: &GrayImage) {}
