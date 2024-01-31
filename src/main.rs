d55fafce139dbeee1278cb0686ee8d02d0c3e6c9
Ilya <mipselqq@gmail.com>
2024-01-31 19:31:42 +0300

    fix: incorrect loading representation, add: illustration

--- src/main.rs
+++ src/main.rs
@@ -24,22 +24,27 @@ struct Args {
 
     #[arg(short, long)]
     output: String,
+
+    #[arg(short, long)]
+    fast: u8,
 }
 
 fn main() {
     let args = Args::parse();
 
     let source_image_path = Path::new(&args.input);
-    let target_width: u32 = args.width;
-    let target_height: u32 = args.height;
-    let output_path = &args.output;
+    let target_width = args.width;
+    let target_height = args.height;
+    let output_path = args.output;
+    let is_fast = args.fast == 1;
 
     let image = image::open(source_image_path).expect("Failed to read the image").into_rgb8();
+    let width = image.width();
     let mut carving_image_view = CarvingImageView::from_image(image);
 
-    let mut prograss_bar = ProgressBar::new(target_width.into());
+    let mut prograss_bar = ProgressBar::new(width as u64 - target_width as u64);
 
-    remove_seams_up_to(&mut carving_image_view, target_width, target_height, false, || {
+    remove_seams_up_to(&mut carving_image_view, target_width, target_height, !is_fast, || {
         prograss_bar.inc();
     });
 
